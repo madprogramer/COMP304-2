7962aa16cbeb1e89a7b62a0c5080ac3cafd2c400
madprogramer <themadprogramer@gmail.com>
2021-05-24 21:27:47 +0300

    Updated pthread_sleep

--- project2.c
+++ project2.c
@@ -1,52 +1,51 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
 #include <time.h>
+/**
+ * pthread_sleep takes an integer number of seconds to pause the current thread
+ * original by Yingwu Zhu
+ * updated by Muhammed Nufail Farooqi
+ * updated by Fahrican Kosar
+ */
+int pthread_sleep(double seconds){
+    pthread_mutex_t mutex;
+    pthread_cond_t conditionvar;
+    if(pthread_mutex_init(&mutex,NULL)){
+        return -1;
+    }
+    if(pthread_cond_init(&conditionvar,NULL)){
+        return -1;
+    }
+
+    struct timeval tp;
+    struct timespec timetoexpire;
+    // When to expire is an absolute time, so get the current time and add
+    // it to our delay time
+    gettimeofday(&tp, NULL);
+    long new_nsec = tp.tv_usec * 1000 + (seconds - (long)seconds) * 1e9;
+    timetoexpire.tv_sec = tp.tv_sec + (long)seconds + (new_nsec / (long)1e9);
+    timetoexpire.tv_nsec = new_nsec % (long)1e9;
+
+    pthread_mutex_lock(&mutex);
+    int res = pthread_cond_timedwait(&conditionvar, &mutex, &timetoexpire);
+    pthread_mutex_unlock(&mutex);
+    pthread_mutex_destroy(&mutex);
+    pthread_cond_destroy(&conditionvar);
+
+    //Upon successful completion, a value of zero shall be returned
+    return res;
+}
+//OUR CODE STARTS HERE
 #include <semaphore.h>
 #include <signal.h>
-#define NULL  __DARWIN_NULL
-#define __DARWIN_NULL ((void *)0)
 #define MAXCOMMENTATORS 400
 #define CURARG 1+2*i
 #define PROBABILITY_RESOLUTION 10000
- /****************************************************************************** 
-  pthread_sleep takes an integer number of seconds to pause the current thread 
-  original by Yingwu Zhu
-  updated by Muhammed Nufail Farooqi
-  *****************************************************************************/
+
 pthread_mutex_t PANELMUTEX;
 pthread_cond_t NEXTSPEAKER[MAXCOMMENTATORS];
 sem_t cTurn, answerDone;
-
-int pthread_sleep (int seconds)
-{
-   pthread_mutex_t mutex;
-   pthread_cond_t conditionvar;
-   struct timespec timetoexpire;
-   if(pthread_mutex_init(&mutex,NULL))
-    {
-      return -1;
-    }
-   if(pthread_cond_init(&conditionvar,NULL))
-    {
-      return -1;
-    }
-   struct timeval tp;
-   //When to expire is an absolute time, so get the current time and add //it to our delay time
-   gettimeofday(&tp, NULL);
-   timetoexpire.tv_sec = tp.tv_sec + seconds; timetoexpire.tv_nsec = tp.tv_usec * 1000;
-
-   pthread_mutex_lock (&mutex);
-   int res =  pthread_cond_timedwait(&conditionvar, &mutex, &timetoexpire);
-   pthread_mutex_unlock (&mutex);
-   pthread_mutex_destroy(&mutex);
-   pthread_cond_destroy(&conditionvar);
-
-   //Upon successful completion, a value of zero shall be returned
-   return res;
-
-}
 
 //GLOBAL VARIABLES
 int N,Q,time_int;
